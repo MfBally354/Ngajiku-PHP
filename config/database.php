eea561079d6a3fd8e6f298cb2ba846a5a43f67f5
iqbal guntur <iqbal@debal.iqbal>
2026-03-22 12:42:02 +0700

    Auto commit

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@ define('DB_PASS', '#iqbaldebian#');
 define('DB_CHARSET', 'utf8mb4');
 
 define('SITE_NAME', 'NgajiKu');
-define('SITE_URL', 'http://192.168.1.18:8080');
+define('SITE_URL', 'http://100.72.99.104:8080');
 define('UPLOAD_PATH', __DIR__ . '/../uploads/');
 define('UPLOAD_URL', SITE_URL . '/uploads/');
 define('MAX_FILE_SIZE', 10 * 1024 * 1024); // 10 MB
