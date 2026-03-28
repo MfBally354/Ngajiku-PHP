03d11ab0c1e94a0b0cb64b13eb141a8e9c3cfe16
MfBally354 <iqbalguntur313@gmail.com>
2026-03-28 12:39:00 +0700

    Auto commit

--- pages/admin/pengumuman.php
+++ pages/admin/pengumuman.php
@@ -110,4 +110,4 @@ require_once '../../includes/header.php';
         </div>
     </div>
 </div>
-<?php require_once '../../includes/header.php'; ?>
+<?php require_once '../../includes/footer.php'; ?>
